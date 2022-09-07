b58877f85471b8824ba802fd6b13b7dd5a5bdd8b
Harsh jangitwar <harshjangitwar@Harshs-MacBook-Air.local>
2022-09-07 20:49:24 +0530

    project completed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,10 @@
-import react from 'react';
-import '../App.css';
+// Footer
+import react from "react";
+import "../App.css";
 const Footer = () => {
   return (
     <>
       <div className="container-fluid bg-dark mt-auto footer">
-
         <footer class="text-center text-white">
           <div class="container pt-4">
             <section class="mb-4">
@@ -13,50 +13,65 @@ const Footer = () => {
                 href="#!"
                 role="button"
                 data-mdb-ripple-color="light"
-              ><i class="fab fa-facebook-f"></i></a>
+              >
+                <i class="fab fa-facebook-f"></i>
+              </a>
 
               <a
                 class="btn btn-link btn-floating btn-lg text-light m-1"
                 href="#!"
                 role="button"
                 data-mdb-ripple-color="light"
-              ><i class="fab fa-twitter"></i></a>
+              >
+                <i class="fab fa-twitter"></i>
+              </a>
 
               <a
                 class="btn btn-link btn-floating btn-lg text-light m-1"
                 href="#!"
                 role="button"
                 data-mdb-ripple-color="light"
-              ><i class="fab fa-google"></i></a>
+              >
+                <i class="fab fa-google"></i>
+              </a>
 
               <a
                 class="btn btn-link btn-floating btn-lg text-light m-1"
                 href="#!"
                 role="button"
                 data-mdb-ripple-color="light"
-              ><i class="fab fa-instagram"></i></a>
+              >
+                <i class="fab fa-instagram"></i>
+              </a>
 
               <a
                 class="btn btn-link btn-floating btn-lg text-light m-1"
                 href="#!"
                 role="button"
                 data-mdb-ripple-color="light"
-              ><i class="fab fa-linkedin"></i></a>
+              >
+                <i class="fab fa-linkedin"></i>
+              </a>
               <a
                 class="btn btn-link btn-floating btn-lg text-light m-1"
                 href="#!"
                 role="button"
                 data-mdb-ripple-color="light"
-              ><i class="fab fa-github"></i></a>
+              >
+                <i class="fab fa-github"></i>
+              </a>
             </section>
           </div>
           <div class="text-center text-light p-3">
             © 2022 Copyright:
-            <a class="text-light" href="#"> crypto.com</a>
+            <a class="text-light" href="#">
+              {" "}
+              crypto.com
+            </a>
           </div>
         </footer>
       </div>
     </>
-  )
-}
+  );
+};
 export default Footer;
